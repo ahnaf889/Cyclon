4f8995c33aecd901be53d9780969af324641e702
ahnaf889 <dreamlessboyahnaf8@gmail.com>
2025-11-26 22:41:05 +0600

    Heeard complate

--- src/components/HomeComponents/Header/Index.tsx
+++ src/components/HomeComponents/Header/Index.tsx
@@ -1,9 +1,22 @@
+import Container from "../../CommenComponents/Container";
+import { assets } from "../../../helpers/AssetsProvider.js";
+
+
 const Header = () => {
     return (
-        <div>
-            This is Header
+        <div className=" bg-gray-900">
+            <Container>
+                <div className=" grid grid-cols-3">
+                    <div className="bg-purple-300">
+                        <img src={assets.Logo} alt="Logo" />
+                    </div>
+                    <div className="bg-red-300">50%</div>
+                    <div className="bg-green-300"><button>Hello </button></div>
+                </div>
+            </Container>
+
         </div>
     )
 }
 
-export default Header
+export default Header;
